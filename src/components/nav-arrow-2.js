185a02a15afd7c5042cc5a64ccc65e1c2e4a41f0
hung <manhhung44556688@gmail.com>
2023-02-27 15:57:54 +0700

    feat: add nav-arrow, nav-arrow-2 components

---
+++ src/components/nav-arrow-2.js
@@ -0,0 +1,12 @@
+export function NavArrow2() {
+  return (
+    <>
+      <button class="nav-arrow-2">
+        <i class="fa-solid fa-chevron-left"></i>
+      </button>
+      <button class="nav-arrow-2 nav-arrow-2__right nav-arrow-2__active">
+        <i class="fa-solid fa-chevron-left"></i>
+      </button>
+    </>
+  );
+}
