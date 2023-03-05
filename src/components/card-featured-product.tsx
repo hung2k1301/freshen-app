39fb29a36abc81a64add6905ec923fc5993b5383
hung <manhhung44556688@gmail.com>
2023-03-05 15:50:01 +0700

    feat: create CardFeaturedProduct

---
+++ src/components/card-featured-product.tsx
@@ -0,0 +1,133 @@
+interface FeaturedProduct {
+  cardActive?: string;
+  behaviourActive?: string;
+  isTag?: boolean;
+  btnActive?: string;
+  title: string;
+  name: string;
+  volume: string;
+  price: string;
+}
+
+const cardFeaturedProduct: FeaturedProduct[] = [
+  {
+    cardActive: "ft-product-card--active",
+    behaviourActive: "ft-product-card__behaviour--active",
+    title: "fruits",
+    name: "pineapple (tropical gold)",
+    volume: "1lb",
+    price: "2.00",
+    btnActive: "ft-product-card__btn--active",
+  },
+  {
+    title: "vegetables",
+    name: "organic rocket",
+    volume: "1lb",
+    price: "2.00",
+  },
+  {
+    isTag: true,
+    title: "food & grocery",
+    name: "silver heinz ketchup",
+    volume: "350ml",
+    price: "7.63",
+  },
+  {
+    title: "food & grocery",
+    name: "augason farms freeze dried",
+    volume: "Beef Chunks (Pack of 6)",
+    price: "64.92",
+  },
+  {
+    title: "fruits",
+    name: "organic rocket",
+    volume: "1 lb",
+    price: "2.00",
+  },
+  {
+    title: "fruits",
+    name: "organic rocket",
+    volume: "1 lb",
+    price: "2.00",
+  },
+  {
+    title: "fruits",
+    name: "silver heinz ketchup",
+    volume: "350 ml",
+    price: "7.63",
+  },
+  {
+    title: "see food",
+    name: "augason farms freeze dried",
+    volume: "Beef Chunks (Pack of 6)",
+    price: "64.92",
+  },
+];
+
+export function CardFeaturedProduct(): JSX.Element {
+  return (
+    <>
+      {cardFeaturedProduct.map((cardData, index) => (
+        <div className="col-span-3 lg:col-span-4 xl:col-span-3" key={index}>
+          <a
+            className={`ft-product-card ${cardData.cardActive} pt-5 xl:pt-[11px] px-0 xl:px-[30px]`}
+            href=""
+          >
+            <div className="ft-product-card__thumbnail">
+              <img
+                src="https://picsum.photos/id/75/200/300"
+                alt="Featured Product"
+                className="ft-product-card__img"
+              />
+
+              {cardData.isTag && (
+                <div className="ft-product-card__tag">
+                  <div className="ft-product-card__common ft-product-card__state">
+                    hot
+                  </div>
+                  <div className="ft-product-card__common ft-product-card__deal">
+                    -4 %
+                  </div>
+                </div>
+              )}
+              <div className="ft-product-card__behaviour {{this.behaviourActive}} hidden xl:flex border-none xl:border-solid">
+                <div className="ft-product-card__save">
+                  <img src="/images/green-heart.svg" alt="Save icon" />
+                </div>
+                <img src="/images/black-search.svg" alt="Search" />
+                <img src="/images/compare-icon.svg" alt="Compare" />
+              </div>
+              <img
+                src="/images/heart.svg"
+                alt="Save icon"
+                className="absolute top-[-18px] right-[-20px] xl:hidden"
+              />
+            </div>
+
+            <p className="ft-product-card__title">{cardData.title}</p>
+
+            <span>
+              <i className="fa-solid fa-star ft-product-card__star"></i>
+              <i className="fa-solid fa-star ft-product-card__star"></i>
+              <i className="fa-solid fa-star ft-product-card__star"></i>
+              <i className="fa-solid fa-star ft-product-card__star"></i>
+              <i className="fa-solid fa-star ft-product-card__star ft-product-card__star--grey"></i>
+            </span>
+
+            <p className="ft-product-card__name">{cardData.name}</p>
+            <p className="ft-product-card__volume">{cardData.volume}</p>
+
+            <div className="ft-product-card__price-container">
+              <p className="ft-product-card__price">${cardData.price}</p>
+
+              <button className="ft-product-card__btn {{this.btnActive}}">
+                <img src="/images/white-cart.svg" alt="cart" />
+                ADD TO CART
+              </button>
+            </div>
+          </a>
+        </div>
+      ))}
+    </>
+  );
+}
