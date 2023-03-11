dcb5d0eeb6edd41dceca5bad8280c6b047231ae9
hung <manhhung44556688@gmail.com>
2023-03-11 17:29:24 +0700

    refactor: optimize nav bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,34 +1,25 @@
+import { INavBar } from "@/types";
+import { getNavbar } from "@/services/api";
+import { useQuery } from "react-query";
+
 export function NavBar(): JSX.Element {
+  const { isLoading, data } = useQuery<INavBar[]>(
+    "home-page-navBar",
+    getNavbar
+  );
+
+  if (isLoading) return <h3>Loading...</h3>;
+
   return (
     <nav className="nav-bar">
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">HOME</span>
-        <i className="fa-solid fa-chevron-down nav-bar__arrow-icon"></i>
-      </a>
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">FRESH FOOD</span>
-      </a>
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">BAKERY</span>
-      </a>
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">DRINKS</span>
-      </a>
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">SHOP</span>
-        <i className="fa-solid fa-chevron-down nav-bar__arrow-icon"></i>
-      </a>
-      <a href="" className="nav-bar__menu nav-bar__menu--active">
-        <span className="nav-bar__name">PAGES</span>
-        <i className="fa-solid fa-chevron-down nav-bar__arrow-icon"></i>
-      </a>
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">BLOG</span>
-        <i className="fa-solid fa-chevron-down nav-bar__arrow-icon"></i>
-      </a>
-      <a href="" className="nav-bar__menu">
-        <span className="nav-bar__name">CONTACT</span>
-      </a>
+      {data?.map((data, index) => (
+        <a href="" className="nav-bar__menu" key={index}>
+          <span className="nav-bar__name">{data.name}</span>
+          {data.arrowIcon && (
+            <i className="fa-solid fa-chevron-down nav-bar__arrow-icon"></i>
+          )}
+        </a>
+      ))}
     </nav>
   );
 }
